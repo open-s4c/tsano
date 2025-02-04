a61c98b663b7e46aaf30d1bbf1a54f7adf9d24d4
Diogo Behrens <diogo.behrens@huawei.com>
2025-02-04 09:12:09 +0000

    Simplify mempool
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

--- include/bingo/mempool.h
+++ include/bingo/mempool.h
@@ -7,12 +7,9 @@
 
 #include <stddef.h>
 
-int mempool_init(size_t cap);
-int mempool_fini(void);
-
 void *mempool_malloc(size_t size);
 void *mempool_calloc(size_t number, size_t size);
 void *mempool_realloc(void *ptr, size_t size);
 void mempool_free(void *ptr);
 
-#endif
+#endif /* BINGO_MEMPOOL_H */
--- src/mempool.c
+++ src/mempool.c
@@ -5,14 +5,6 @@
 #include <bingo/mempool.h>
 #include <stdlib.h>
 
-int
-mempool_init(size_t cap)
-{
-}
-int
-mempool_fini(void)
-{
-}
 
 void *
 mempool_malloc(size_t size)
