39bc0dd9f83741c5a8775de94cab30904e6a2ada
Diogo Behrens <diogo.behrens@huawei.com>
2025-02-12 12:44:06 +0000

    mod/autocept: Introduce auto interceptor
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

---
+++ mod/autocept.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ */
+#include "autocept.h"
+#include <bingo/intercept.h>
+#include <bingo/module.h>
+#include <bingo/real.h>
+
+BINGO_MODULE_INIT()
+
+// TODO: add function cache
+
+void *
+autocept_before(const char *name)
+{
+    autocept_event_t ev = {.fname = name};
+    intercept_before(EVENT_AUTOCEPT, &ev, 0);
+    return real_func(name, 0);
+}
+
+void
+autocept_after(void)
+{
+    intercept_after(EVENT_AUTOCEPT, 0, 0);
+}
---
+++ mod/autocept.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef BINGO_AUTOCEPT_H
+#define BINGO_AUTOCEPT_H
+
+#define EVENT_AUTOCEPT 3000
+
+typedef struct {
+    const char *fname;
+} autocept_event_t;
+
+#endif /* BINGO_AUTOCEPT_H */
