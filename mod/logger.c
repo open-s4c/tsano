246b22c01f1d563d4fb29c53084afc2e7ab1bbd0
Diogo Behrens <diogo.behrens@huawei.com>
2025-02-12 12:55:18 +0000

    mod/logger: Introduce simple logger module
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

---
+++ mod/logger.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ */
+#include <pthread.h>
+
+#include <bingo/log.h>
+#include <bingo/pubsub.h>
+
+#define ADD_LOGGER(CHAIN)                                                      \
+    PS_SUBSCRIBE(CHAIN, {                                                      \
+        char *chain = #CHAIN;                                                  \
+        log_printf("%s\t%u[%lx]\n", chain, event, (uint64_t)pthread_self());   \
+    })
+
+ADD_LOGGER(INTERCEPT_BEFORE)
+ADD_LOGGER(INTERCEPT_AFTER)
+ADD_LOGGER(INTERCEPT_AT)
+
+BINGO_MODULE_INIT()
