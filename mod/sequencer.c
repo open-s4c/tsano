bbc59001290adc4912d4efb3de8112e8a9957863
Diogo Behrens <diogo.behrens@huawei.com>
2025-02-14 07:16:57 +0000

    mod: Add trivial sequencer
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

---
+++ mod/sequencer.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * SPDX-License-Identifier: MIT
+ */
+#include <bingo/log.h>
+#include <bingo/pubsub.h>
+#include <bingo/self.h>
+#include <bingo/switcher.h>
+
+PS_SUBSCRIBE(INTERCEPT_AT, {
+    switch (event) {
+        case EVENT_THREAD_FINI:
+            switcher_wake(ANY_THREAD, 0);
+            break;
+        case EVENT_THREAD_INIT:
+            /* threads call this only ONCE (except the main thread). */
+            switcher_yield(self_id(), true);
+            break;
+        default:
+            break;
+    }
+    return false;
+})
+
+PS_SUBSCRIBE(INTERCEPT_BEFORE, {
+    switcher_wake(ANY_THREAD, 0);
+    return false;
+})
+
+PS_SUBSCRIBE(INTERCEPT_AFTER, {
+    switcher_yield(self_id(), true);
+    return false;
+})
+
+BINGO_MODULE_INIT()
