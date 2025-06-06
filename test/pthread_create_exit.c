aa70d2677c066d8b5fdcbe4760c54fbb54fb7cfd
Diogo Behrens <diogo.behrens@huawei.com>
2025-06-06 07:46:41 +0200

    test: Add pthread_create and related tests
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

---
+++ test/pthread_create_exit.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <pthread.h>
+
+#include <dice/intercept/pthread.h>
+#include <dice/thread_id.h>
+
+int init_called;
+int fini_called;
+int run_called;
+
+PS_SUBSCRIBE(INTERCEPT_EVENT, EVENT_THREAD_INIT, { init_called++; })
+PS_SUBSCRIBE(INTERCEPT_EVENT, EVENT_THREAD_FINI, { fini_called++; })
+
+void *
+run()
+{
+    run_called++;
+    pthread_exit(0);
+    return 0;
+}
+
+int
+main()
+{
+    pthread_t t;
+    pthread_create(&t, 0, run, 0);
+    pthread_join(t, 0);
+
+    assert(run_called == 1);
+    assert(init_called == 1);
+    assert(fini_called == 1);
+
+    return 0;
+}
