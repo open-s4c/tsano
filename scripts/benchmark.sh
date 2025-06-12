9ab479ca4dacbd4d751010f222b78f5ee89d8cc0
Diogo Behrens <diogo.behrens@huawei.com>
2025-06-12 16:40:36 +0200

    bench: Script to run all benchmarks
    Signed-off-by: Diogo Behrens <diogo.behrens@huawei.com>

---
+++ scripts/benchmark.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+# Copyright (C) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+# SPDX-License-Identifier: MIT
+#
+set -e
+
+OPTION_CLEAN=no
+OPTION_CONFIG=no
+OPTION_BUILD=no
+OPTION_RUN_MICRO=no
+OPTION_SUM_MICRO=yes
+OPTION_RUN_LEVELDB=yes
+OPTION_SUM_LEVELDB=yes
+OPTION_WIKI=yes
+WIKI_URL=ssh://git@github.com/open-s4c/dice.wiki.git
+
+enabled() {
+    if [ -z "$1" ] || [ "$1" != "yes" ]
+    then return 1
+    else return 0
+    fi
+}
+
+# CONFIG AND BUILD DICE
+
+if enabled $OPTION_CLEAN; then
+    echo rm -rf build
+fi
+
+if enabled $OPTION_CONFIG; then
+    cmake -S . -Bbuild -DCMAKE_BUILD_TYPE=Release
+fi
+
+if enabled $OPTION_BUILD; then
+    cmake --build build
+fi
+
+# RUN BENCHMARKS
+
+if enabled $OPTION_RUN_MICRO; then
+    make -sC bench/micro run
+fi
+
+if enabled $OPTION_RUN_LEVELDB; then
+    make -sC bench/leveldb run
+fi
+
+# SUMMARY
+
+DATE=$(date "+%Y-%m-%d")
+HOST=$(hostname)
+SUMMARY=bench-$HOST-$DATE.md
+
+rm -f $SUMMARY
+
+output() {
+    echo "$@" >> $SUMMARY
+}
+
+output "# Dice Benchmarks"
+output
+output "- Host: $HOST"
+output "- Date: $DATE"
+
+if enabled $OPTION_SUM_MICRO; then
+    make -sC bench/micro process
+
+    output
+    output "## Microbenchmarks"
+    cat bench/micro/work/results.csv | mlr --icsv --omd cat >> $SUMMARY
+fi
+
+if enabled $OPTION_SUM_LEVELDB; then
+    make -sC bench/leveldb process
+
+    output
+    output "## LevelDB"
+    cat bench/leveldb/work/results.csv \
+    | mlr --icsv --ifs=\; --omd cat >> $SUMMARY
+fi
+
+
+# CLONE WIKI
+
+if enabled $OPTION_WIKI; then
+
+    if [ ! -d dice.wiki ]; then
+        git clone WIKI_URL
+    fi
+
+    if [ -d dice.wiki ]; then
+        (cd dice.wiki && git pull)
+    fi
+
+    mv $SUMMARY dice.wiki/bench
+
+fi
+
